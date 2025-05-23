737cfeae6ac7a54eb3d66b44fcb948f6d5678f42
Yogi20-04 <Yogesh.Ankamwar@nttdata.com>
2025-05-23 23:28:04 +0530

    Ygesh Update

--- force-app/main/default/lwc/newTest/newTest.js
+++ force-app/main/default/lwc/newTest/newTest.js
@@ -3,4 +3,5 @@ import { LightningElement } from 'lwc';
 export default class NewTest extends LightningElement {}
 //hello 
 //I have Updated This Code Plase Check
-//Again I have Update 
+//Again I have Update
+//Last One 
